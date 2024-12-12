aef1694b5010d1519037c0b3d29b58e1744c14e3
mudde-dev <mudde.dev@gmail.com>
2024-12-12 09:31:10 +0100

    configured solution, program.cs to initiate DI and imported Layout.cshtml from Appstudies

---
+++ GoodFriends_ProjectB-RazorPages/Program.cs
@@ -0,0 +1,46 @@
+using Services;
+using Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+
+//use multiple Secret sources, Database connections and their respective DbContexts
+object value = builder.Configuration.AddApplicationSecrets("../Configuration/Configuration.csproj");
+object value1 = builder.Services.AddDatabaseConnections(builder.Configuration);
+
+#region Setup the Dependency service
+builder.Services.AddScoped<IFriendsService, FriendsServiceDb>();
+builder.Services.AddHttpClient(name: "MusicWebApi", configureClient: options =>
+{
+    options.BaseAddress = new Uri(builder.Configuration["DataService:WebApiBaseUri"]);
+    options.DefaultRequestHeaders.Accept.Add(
+        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(
+            mediaType: "application/json",
+            quality: 1.0));
+});
+#endregion
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
